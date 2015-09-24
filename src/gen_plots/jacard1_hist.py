04b09a073efa922777f17ad844bcf99f4276d44a
gSchool Student <datascientist@Datas-Mac-mini-7.local>
2015-09-23 19:01:03 -0600

    updated jacard1_hist

--- src/gen_plots/jacard1_hist.py
+++ src/gen_plots/jacard1_hist.py
@@ -16,12 +16,12 @@ def dependency_hist(save=True, show=False, verbose=False, bins=0):
   jacList = get_2highest_jacard()
   if bins==0:
     unique = list(set(jacList))
-    bins = int(len(unique) ** 0.5) * 2
+    bins = int(len(unique) ** 0.5)# * 2
   if verbose:
     print('max best similarity is: ' + str(max(jacList)))
     print('min best similarity is: ' + str(min(jacList)))
-  plt.title('similarity of top match')
-  plt.xlabel('similarity fraction')
+  plt.title('similarity of top match over all packages')
+  plt.xlabel('jacard similarity')
   plt.ylabel('number of packages')
   plt.hist(jacList, bins=bins)
   if save: plt.savefig(outputDir + '/' + 'jacard1_hist.png', dpi=720)
