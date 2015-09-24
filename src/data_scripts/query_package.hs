b1fa037395b5946d598097e35819978aab8b29a8
gSchool Student <datascientist@Datas-Mac-mini-7.local>
2015-09-23 18:48:33 -0600

    terminal app is working correctly

--- src/data_scripts/query_package.hs
+++ src/data_scripts/query_package.hs
@@ -16,6 +16,22 @@ searchPr pr n = head $ filter (\(a,b) -> a==n) pr
 showPr :: (String,Double) -> String
 showPr (n,p) = n ++ " has pagerank: " ++ (show p)
 
+searchPrPercentileFrac :: PageRanks -> String -> (String,Double)
+searchPrPercentileFrac p s = (s, (fromIntegral $ length $ filter (<f) $ map snd p)
+    / (fromIntegral $ length p ::Double))
+  where f = snd $ searchPr p s
+
+searchPrPercentile :: PageRanks -> String -> (String,Double)
+searchPrPercentile p s = (a, 100*b)
+  where (a,b) = searchPrPercentileFrac p s
+
+showPrPercentile :: (String,Double) -> String
+showPrPercentile (n,p) = n ++ " has pagerank percentile: " ++ (show p)
+
+indentation :: PageRanks -> String -> String
+indentation p s = replicate i '-'
+  where i = fromIntegral $ floor $ (snd $ searchPrPercentile p s) / 10
+
 type Similarities = [(String, [(String, Double)])]
 
 getSimilarities :: IO Similarities
@@ -33,16 +49,25 @@ getSimilarities = do
 searchSim::Similarities->String->(String,[(String,Double)])
 searchSim s n = head $ filter (\e -> fst e == n) s
 
-showSim :: (String, [(String,Double)]) -> String
-showSim (h,t) = unlines $ h:" has similar packages":
+makeLength :: Int -> String -> String
+makeLength n s
+  | length s < n = s ++ (replicate (n - (length s)) ' ')
+  | otherwise = take n s
+
+showSim :: PageRanks -> (String, [(String,Double)]) -> String
+showSim p (h,t) = unlines $ (h ++ " has similar packages:"):
     (map showSingleSim t)
   where showSingleSim :: (String,Double)->String
-        showSingleSim (s,d) = "  " ++ s ++ " has similarity: " ++ (show d)-- ++ " %"
+        showSingleSim (s,d) = " " ++ s ++ minusmax ++ " " ++ indent ++ "- " ++ " has similarity: " ++ (makeLength 4 $ show $ d) ++ " and pagerank-percentile: " ++ (makeLength 4 $ show $ snd $ searchPrPercentile p s)
+          where indent = indentation p s
+                maxlength = maximum $ map (\(s,d) -> (length s) + (length $ indentation p s)) t :: Int
+                minusmax = replicate (maxlength - (length s) - (length indent)) ' ' :: String
 
 printPackageData::PageRanks->Similarities->String->IO ()
 printPackageData p s n = do
   putStrLn $ showPr $ searchPr p n
-  putStr $ showSim $ searchSim s n
+  putStrLn $ showPrPercentile $ searchPrPercentile p n
+  putStr $ showSim p $ searchSim s n
 
 querySimN :: PageRanks -> Similarities -> IO ()
 querySimN p s = do
